b273967b82e153ceee2ea3d4da2a60beb281bd46
Michał <m.tyczynski1993@gmail.com>
2022-03-29 06:44:45 +0200

    Jinja templates

--- app/views.py
+++ app/views.py
@@ -9,7 +9,48 @@ def index():
 def jinja():
 
     my_name = "Michal"
-    return render_template("public/jinja.html", my_name=my_name)
+    age = 30
+    
+    langs = ['Python', 'Java', 'JavaScript', 'Bash', 'HTML', 'C', 'Ruby']
+
+    friends = {
+        'Tomek': 25,
+        'Maja': 30,
+        'Iza': 29,
+        'Grzegorz': 20
+    }
+
+    colours = ('Red', 'Green')
+
+    cool = True
+
+    class GitRemote():
+        def __init__(self, name, description, url):
+            self.name = name
+            self.description = description
+            self.url = url
+        
+        def pull(self):
+            return f'Pulling repo {self.name}'
+        
+        def clone(self):
+            return f'Cloning repo {self.url}'
+    
+    my_remote = GitRemote(
+        name= 'Flask Tutorial', 
+        description='Template design tutorial', 
+        url='https://github.com/julian-nash/jinja.git')
+        
+    
+    def repeat(x, qty):
+        return x * qty
+
+    
+    return render_template(
+        "public/jinja.html", my_name=my_name, age=age, 
+        langs=langs, friends=friends, colours=colours, 
+        cool=cool, GitRemote=GitRemote, repeat=repeat, 
+        my_remote=my_remote)
 
 @app.route("/about")
 def about():
